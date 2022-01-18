47cbd27455026323f380ffc7a9addd213ec5c1ae
Prathik Pugazhenthi <prathik.pappu@yahoo.co.in>
2022-01-18 16:31:55 +0530

    app edit -1

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -8,10 +8,10 @@ export default function DailyForecast({minTemp,maxTemp,weather}) {
     const weatherIcon = weather==="Clear" || weather.includes("Sun")?<WiDaySunny/>:weather==="Drizzle" || weather.includes("Rain")?<WiDayRain/>:weather.includes("Cloud")|| weather.includes("Mist") || weather.includes("Haze")?<WiCloudy/>:weather.includes("Thunder")?<WiDayThunderstorm/>:<WiDaySnow/>;
 
     const render=
-    <div className='cardWrapper1 my-5'>
-        <div className='cardDiv1'>
-            <div className='cardIcon1'>{weatherIcon}</div>
-            <div className='cardTemp1'>
+    <div className='cardWrapperDaily my-5'>
+        <div className='cardDivDaily'>
+            <div className='cardIconDaily'>{weatherIcon}</div>
+            <div className='cardTempDaily'>
                 <div className='cardTempExpand'>
                     <div>Min.</div>
                     <div>{Math.round(minTemp)} <span>&#8451;</span></div>
--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -7,10 +7,10 @@ export default function HourlyForecast({temperature,weather}) {
     const weatherIcon = weather==="Clear" || weather.includes("Sun")?<WiDaySunny/>:weather==="Drizzle" || weather.includes("Rain")?<WiDayRain/>:weather.includes("Cloud")|| weather.includes("Mist") || weather.includes("Haze")?<WiCloudy/>:weather.includes("Thunder")?<WiDayThunderstorm/>:<WiDaySnow/>;
 
     const render = 
-    <div className='cardWrapper2 my-5'>
-        <div className='cardDiv2'>
-            <div className='cardIcon2'>{weatherIcon}</div>
-            <div className='cardTemp2'>{Math.round(temperature)}<span>&#8451;</span></div>
+    <div className='cardWrapperHourly my-5'>
+        <div className='cardDivHourly'>
+            <div className='cardIconHourly'>{weatherIcon}</div>
+            <div className='cardTempHourly'>{Math.round(temperature)}<span>&#8451;</span></div>
         </div>
     </div>
     
